8797faead24d7084b6aa3c401717e2ba03ba891d
zhouyang.1107 <zhouyang.1107@bytedance.com>
2026-01-20 13:46:54 +0800

    测速

--- vllm_fowarder/vllm.sh
+++ vllm_fowarder/vllm.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+export TIKTOKEN_ENCODINGS_BASE="/mnt/hdfs/__MERLIN_USER_DIR__/models/encodings"
+
+pkill -f "vllm serve"
 # ===== 配置参数 =====
-TP_SIZE=8   # 可选 1 / 2 / 4 / 8
-BASE_PORT=8101
+TP_SIZE=1   # 可选 1 / 2 / 4 / 8
+BASE_PORT=9003
 
 # 模型路径列表（根据TP_SIZE自动裁剪）
 MODEL_PATHS=(
-/lpai/dataset/model/0-1-0/model/Qwen3-235B-A22B-Thinking-2507
+/mnt/hdfs/__MERLIN_USER_DIR__/models/gpt-oss-120b
+/mnt/hdfs/__MERLIN_USER_DIR__/models/gpt-oss-120b
+/mnt/hdfs/__MERLIN_USER_DIR__/models/gpt-oss-120b
+/mnt/hdfs/__MERLIN_USER_DIR__/models/gpt-oss-120b
 )
 
 # ===== 脚本逻辑 =====
-NUM_GPUS=8
-MAX_MODELS=$((NUM_GPUS / TP_SIZE))
+AVAILABLE_GPUS=(4 5 6 7)
+MAX_MODELS=$(( ${#AVAILABLE_GPUS[@]} / TP_SIZE ))
 
 # 只取前 MAX_MODELS 个模型
 MODEL_PATHS=("${MODEL_PATHS[@]:0:$MAX_MODELS}")
@@ -24,18 +30,19 @@ for i in "${!MODEL_PATHS[@]}"; do
     PORT=$((BASE_PORT + i))
     GPU_START=$((i * TP_SIZE))
     GPU_END=$((GPU_START + TP_SIZE - 1))
-    
+
     # 拼接 GPU id 列表
-    GPUS=$(seq -s, $GPU_START $GPU_END)
+    GPUS_ARR=("${AVAILABLE_GPUS[@]:$GPU_START:$TP_SIZE}")
+    IFS=, GPUS="${GPUS_ARR[*]}"
     MODEL_PATH="${MODEL_PATHS[$i]}"
     
     echo "启动模型服务: $MODEL_PATH 端口: $PORT 使用GPU: $GPUS (TP=$TP_SIZE)"
     
     CUDA_VISIBLE_DEVICES=$GPUS vllm serve "$MODEL_PATH" \
         --port $PORT \
-        --host 0.0.0.0 \
+        --host :: \
         --tensor-parallel-size $TP_SIZE \
-        --served-model-name default \ &
+        --served-model-name default &
 
 done
 
